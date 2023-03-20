974aa12c30a0a18a8141d6d95266839afbe260aa
Lazazarus <mlazarus3471@sdsu.edu>
2023-03-19 17:09:31 -0700

    outline
    Feel free to edit straight onto this

--- DSP.C
+++ DSP.C
@@ -1,2 +1,24 @@
 // this file will be an outline of the DSP functions
 
+// create buffers for data storage
+
+//interupts
+
+//sampling at 44.1 khz & incriment sample counter
+
+
+// when enoguh samples have been made, do an fft
+
+
+// once fft is done, figure out which afrequencies are ascociated with the top 5 peaks
+// remad up on what the fft algorithum is going to output and if we are going to need to convert that to Hz
+//or if we can figure out how to just use whatever comes out of it to do the calcs
+
+// with those 5 highest peak frequencies, figure out which note is closest to a "known note"
+
+//put that frequency in lookup table then output answer to GUI
+
+// the best embedded systems are all interrupts with nothing in the main function
+
+
+
