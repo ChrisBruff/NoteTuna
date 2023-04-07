1da06f3c20d8d77468f341f680368e090fd79d4b
rcontreras2309 <128092533+rcontreras2309@users.noreply.github.com>
2023-04-06 20:59:03 -0700

    Sequential sampling Fs = 3 kHz,  N = 4096

---
+++ Senior_Design_TestBench1/src/Sequential_sampling.cpp
@@ -0,0 +1,37 @@
+#include <arduinoFFT.h>
+#include <DacESP32.h>
+
+#define SAMPLES 4096
+#define SAMPLING_FREQUENCY 3000
+
+unsigned int sampling_period_us;
+unsigned long newTime;
+double vReal[SAMPLES];
+double vImag[SAMPLES];
+
+DacESP32 dac1(GPIO_NUM_25);
+arduinoFFT FFT = arduinoFFT(vReal, vImag, SAMPLES, SAMPLING_FREQUENCY);
+
+void setup(){
+  Serial.begin(115200);
+  analogReadResolution(12);
+  analogSetPinAttenuation(GPIO_NUM_34, ADC_11db);
+  pinMode(GPIO_NUM_34, INPUT);
+  sampling_period_us = round(1000000 * 1 / SAMPLING_FREQUENCY);
+  dac1.outputCW(440);
+}
+
+void loop(){
+  for (int i = 0; i < SAMPLES; i++) {
+    newTime = micros();
+    vReal[i] = analogRead(GPIO_NUM_34); // A conversion takes about 9.7uS on an ESP32
+    vImag[i] = 0;
+    while ((micros() - newTime) < sampling_period_us){}
+  }
+
+  FFT.Windowing(FFT_WIN_TYP_RECTANGLE, FFT_FORWARD);
+  FFT.Compute(FFT_FORWARD);
+
+  Serial.print(FFT.MajorPeak());
+  Serial.println(" Hz");
+}
